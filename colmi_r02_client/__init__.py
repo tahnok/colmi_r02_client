5b11453ccafed558b56e24dda505113092effdf9
Wesley Ellis <tahnok@gmail.com>
2024-09-07 13:33:05 -0400

    Add pdoc for documentation

---
+++ colmi_r02_client/__init__.py
@@ -0,0 +1,7 @@
+"""
+This is a python client for the Colmi R02 Smart ring.
+
+You can find it for like ~25USD$ on Aliexpress.
+
+[Hackaday coverage](https://hackaday.com/2024/06/16/new-part-day-a-hackable-smart-ring/)
+"""
