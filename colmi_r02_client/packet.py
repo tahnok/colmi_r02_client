b4d8fb1a7006d125701da26f8c0843b83587d6f9
Wesley Ellis <tahnok@gmail.com>
2024-09-13 18:14:17 -0400

    test: more make_packet tests, rename crc to checksum

--- colmi_r02_client/packet.py
+++ colmi_r02_client/packet.py
@@ -7,19 +7,25 @@ def make_packet(command: int, sub_data: bytearray | None = None) -> bytearray:
     command must be between 0 and 255 (inclusive)
     sub_data must have a len between 0 and 14
     """
-    assert 0 <= command <= 255
+    assert 0 <= command <= 255, "Invalid command, must be between 0 and 255"
     packet = bytearray(16)
     packet[0] = command
 
     if sub_data:
-        assert len(sub_data) <= 14
+        assert len(sub_data) <= 14, "Sub data must be less than 14 bytes"
         for i in range(len(sub_data)):
             packet[i + 1] = sub_data[i]
 
-    packet[-1] = crc(packet)
+    packet[-1] = checksum(packet)
 
     return packet
 
 
-def crc(packet: bytearray) -> int:
+def checksum(packet: bytearray) -> int:
+    """
+    Packet checksum
+
+    Add all the bytes together modulus 255
+    """
+
     return sum(packet) & 255
--- tests/test_packet.py
+++ tests/test_packet.py
@@ -1,12 +1,45 @@
+import pytest
+
 from hypothesis import given
 import hypothesis.strategies as st
 
-from colmi_r02_client.packet import make_packet, crc
+from colmi_r02_client.packet import make_packet, checksum
 
 
 @given(command=st.integers(min_value=0, max_value=255), sub_data=st.binary(max_size=14))
 def test_make_packet_works_on_valid_data(command, sub_data):
-    packet = make_packet(command, sub_data)
+    packet = make_packet(command, bytearray(sub_data))
 
     assert len(packet) == 16
-    assert packet[-1] == crc(packet[0:15])
+    assert packet[-1] == checksum(packet[0:15])
+
+
+@given(command=st.integers().filter(lambda x: x < 0 or x > 256))
+def test_make_packet_raises_on_bad_command(command):
+    with pytest.raises(AssertionError):
+        make_packet(command)
+
+
+@given(st.binary(min_size=15))
+def test_make_packet_raises_on_too_long_sub_data(sub_data):
+    with pytest.raises(AssertionError):
+        make_packet(1, bytearray(sub_data))
+
+
+@given(st.binary(min_size=1, max_size=14))
+def test_make_packet_includes_sub_data(sub_data):
+    s = bytearray(sub_data)
+
+    p = make_packet(1, s)
+
+    assert p[1 : 1 + len(s)] == s
+
+
+def test_sample_checksum():
+    message = bytearray(b"\x15\x00\x18\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+    assert checksum(message) == 0x32
+
+
+@given(st.binary())
+def test_checksum(message):
+    assert 0 <= checksum(bytearray(message)) < 256
