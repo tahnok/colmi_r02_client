9717f57f7179869ceec5b3f951701e19d6e600c1
rdmm123 <robedanilo2001@gmail.com>
2024-10-23 23:10:05 -0400

    Add general real time readings to client and cli

--- colmi_r02_client/real_time/enum.py
+++ colmi_r02_client/real_time/enum.py
@@ -7,28 +7,31 @@ class Action(IntEnum):
     STOP = 4
 
 class RealTimeReading(IntEnum):
+    """
+    Taken from https://colmi.puxtril.com/commands/#data-request
+    """
     HEART_RATE = 1
     BLOOD_PRESSURE = 2
     SPO2 = 3
     FATIGUE = 4
     HEALTH_CHECK = 5
-    REAL_TIME_HEART_RATE = 6
+    # leaving this out as it's redundant
+    # REAL_TIME_HEART_RATE = 6
     ECG = 7
     PRESSURE = 8
     BLOOD_SUGAR = 9
     HRV = 10
 
 class RealTimeName(StrEnum):
-    HEART_RATE = "heart-rate"
-    SPO2 = "spo2"
-    BLOOD_PRESSURE = "blood-pressure"
-    FATIGUE = "fatigue"
-    HEALTH_CHECK = "health-check"
-    REAL_TIME_HEART_RATE = "real-time-heart-rate"
-    ECG = "ecg"
-    PRESSURE = "pressure"
-    BLOOD_SUGAR = "blood-sugar"
-    HRV = "hrv"
+    HEART_RATE = "heart-rate" # Works for R02
+    SPO2 = "spo2" # Works for R02
+    BLOOD_PRESSURE = "blood-pressure" # Works for R02 (not sure if it's accurate)
+    FATIGUE = "fatigue" # Works for R02 (not sure if it's accurate)
+    HEALTH_CHECK = "health-check" # Works for R02 (not sure if it's accurate)
+    ECG = "ecg" # Works for R02 (not sure if it's accurate)
+    PRESSURE = "pressure" # Works for R02 (not sure if it's accurate)
+    BLOOD_SUGAR = "blood-sugar" # Works for R02 (not sure if it's accurate)
+    HRV = "hrv" # Works for R02 (not sure if it's accurate)
 
 REAL_TIME_MAPPING = {
     RealTimeName.HEART_RATE: RealTimeReading.HEART_RATE,
@@ -36,7 +39,6 @@ REAL_TIME_MAPPING = {
     RealTimeName.SPO2: RealTimeReading.SPO2,
     RealTimeName.FATIGUE: RealTimeReading.FATIGUE,
     RealTimeName.HEALTH_CHECK: RealTimeReading.HEALTH_CHECK,
-    RealTimeName.REAL_TIME_HEART_RATE: RealTimeReading.REAL_TIME_HEART_RATE,
     RealTimeName.ECG: RealTimeReading.ECG,
     RealTimeName.PRESSURE: RealTimeReading.PRESSURE,
     RealTimeName.BLOOD_SUGAR: RealTimeReading.BLOOD_SUGAR,
